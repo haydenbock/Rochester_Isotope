841b559f22e0bc466c52615999b6199141325715
haydenbock <hwb44@cornell.edu>
2023-09-12 10:05:38 -0400

    .

---
+++ Analysis_Script.R
@@ -0,0 +1,15 @@
+# Package Management -----
+install.packages(c("dplyr", "tidyverse", "vegan", "SIBER"))
+
+library(dplyr)
+library(tidyverse)
+library(vegan)
+library(SIBER)
+library(readxl)
+library(readr)
+
+#data -----
+Env <- read_excel("Environmental_Data.xlsx")
+Isotope <- read_excel("Isotope_Data.xlsx")
+
+
