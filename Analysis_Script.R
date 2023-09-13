a9895b419030017279bec40d096e86745e88d381
haydenbock <hwb44@cornell.edu>
2023-09-13 12:40:36 -0400

    updates with basic visualization.

--- Analysis_Script.R
+++ Analysis_Script.R
@@ -13,7 +13,32 @@ Env <- read_excel("Environmental_Data.xlsx")
 Isotope <- read_excel("Isotope_Data.xlsx")
 Joined_DF <- left_join(Env, Isotope)
 
-#Basic Plots ----
+#Basic Visualization ----
+Full.scatter <- Joined_DF %>% ggplot(aes(x = DELTA_13C_vs_IntStandard, y = DELTA_15N_vs_air, color = Taxa, shape = Urban_Kmeans_Cluster)) +
+                  geom_point()
+Full.scatter
+
+#Oribatid - omnivore/opportunist
+Oribatid.scatter <- Joined_DF %>% filter(Taxa == "Oribatid") %>% ggplot(aes(x = DELTA_13C_vs_IntStandard, y = DELTA_15N_vs_air, color = Urban_Kmeans_Cluster)) +
+  geom_point()
+
+Oribatid.scatter 
+
+#Collembola - Fungivore
+Collembola.scatter <- Joined_DF %>% filter(Taxa == "Collembola") %>% ggplot(aes(x = DELTA_13C_vs_IntStandard, y = DELTA_15N_vs_air, color = Urban_Kmeans_Cluster)) +
+  geom_point()
+
+Collembola.scatter
+
+#Mesostigmata - predator
+Mesostigmata.scatter <- Joined_DF %>% filter(Taxa == "Mesostigmata") %>% ggplot(aes(x = DELTA_13C_vs_IntStandard, y = DELTA_15N_vs_air, color = Urban_Kmeans_Cluster)) +
+  geom_point()
+
+Mesostigmata.scatter
+
+
+
+
 
 
 #Analysis -----
